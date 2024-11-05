4c7a7f6e7539a4217dc1b14246f680d9cd5edeea
rezamortasefi <rezamortasefi@gmail.com>
2024-11-05 19:16:02 +0700

    Add files via upload

---
+++ 24051214220_Reza Mortasefi_Tugas 2 Kelompok Belajar C++.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int a, b ;
+	
+//	cout = untuk memunculkan input di hasil output 
+    cout << "input nilai a: "; 
+	cin >> a;
+	
+	cout << "input nilai b: ";
+	cin >> b;
+	
+//	cin = 
+	cout << "Hasil a + b: " << a + b << endl;
+	cout << "Hasil a - b: " << a - b << endl;
+	cout << "Hasil a * b: " << a * b << endl;
+	cout << "Hasil a / b: " << a / b << endl;
+	cout << "Hasil a % b: " << a % b << endl;
+	
+	return 0;
+	
+}
